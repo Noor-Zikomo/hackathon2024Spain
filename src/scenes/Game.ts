2057e52fa1d90e81bdb0d2b3fed328cc613d4fb3
Daniel Mojonero <daniel.mojonero@kps.com>
2024-09-19 12:17:13 +0200

    Prettier

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,28 +1,22 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+export class Game extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
+  background: Phaser.GameObjects.Image;
+  msg_text: Phaser.GameObjects.Text;
 
-    constructor ()
-    {
-        super('Game');
-    }
+  constructor() {
+    super("Game");
+  }
 
-    handleMap ()
-    {
+  handleMap() {}
 
-    }
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x00ff00);
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
-
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-        this.handleMap();
-    }
+    this.background = this.add.image(512, 384, "background");
+    this.background.setAlpha(0.5);
+    this.handleMap();
+  }
 }
