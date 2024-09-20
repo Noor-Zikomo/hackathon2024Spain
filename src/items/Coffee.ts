443b675dacd3af5afa14899e41a578bcbb20a5e5
Jorge Barbancho <jorge.barbancho@kps.com>
2024-09-20 14:19:41 +0200

    Set speed limit

--- src/items/Coffee.ts
+++ src/items/Coffee.ts
@@ -10,13 +10,17 @@ export class Coffee extends Item {
   public onCollision(item: Phaser.Physics.Arcade.Image, player: Character) {
     super.onCollision(item, player);
     const originalSpeed = player.stats.speed;
-    player.setStats({
-      ...player.stats,
-      speed: originalSpeed + configuration["coffee"]["speedBoost"],
-    });
+    let speedWasModified = false;
+    if (player.stats.speed < configuration["player"]["maxSpeed"]) {
+      player.setStats({
+        ...player.stats,
+        speed: originalSpeed + configuration["coffee"]["speedBoost"],
+      });
+      speedWasModified = true;
+    }
     player.scene.time.delayedCall(configuration["coffee"]["delay"], () => {
       const modifiedSpeed = player.stats.speed;
-      if (player.stats.speed > configuration["player"]["maxSpeed"]) {
+      if (speedWasModified) {
         player.setStats({
           ...player.stats,
           speed: modifiedSpeed - configuration["coffee"]["speedBoost"],
